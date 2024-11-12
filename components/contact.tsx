33dfa6d617d2f29f8380538069f70d821c79b882
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2024-11-13 00:41:37 +0500

    fixing-errors

--- components/contact.tsx
+++ components/contact.tsx
@@ -3,35 +3,39 @@ import './style.css'
 
 const Contact = () => {
   return (
-    <div className='container'>
+    <div className="container">
+      <div className="flex flex-col gap-1">
+        <h1 className="text-[#B6BABF]">Bosh sahifa / Qayta Aloqa /</h1>
+        <h2 className="text-[35px] font-bold mt-4">Qayta Aloqa</h2>
+        <p className="text-[18px] text-[#B6BABF]">
+          Bizning ishimiz haqidagi fikir mulohazalaringiz bilan bo&apos;lishing yoki izohlar maydonida o&apos;zingizni qiziqtirgan savolingizni yo&apos;llang
+        </p>
+      </div>
+      <form className="grid grid-cols-1 gap-2 mt-11 md:grid-cols-2 lg:w-[60%]">
         <div className="flex flex-col gap-1">
-        <h1 className='text-[#B6BABF]'>Bosh sahifa / Qayta Aloqa /</h1>
-        <h2 className='text-[35px] font-bold mt-4'>Qayta Aloqa</h2>
-        <p className='text-[18px] text-[#B6BABF]'>Bizning ishimiz haqidagi fikir mulohazalaringiz bilan bo’lishing yoki izohlar maydonida o’zingizni qiziqtirgan savolingizni yo’llang</p>
+          <label className="text-black">Ism</label>
+          <input type="text" className="w-full h-[45px] rounded-md outline-none p-1 bg-[#EBEFF3]" />
         </div>
-        <form className='grid grid-cols-1 gap-2 mt-11 md:grid-cols-2 lg:w-[60%]'>
-            <div className="flex flex-col gap-1">
-                <label className='text-black'>Ism</label>
-                <input type="text" className='w-full h-[45px] rounded-md outline-none p-1 bg-[#EBEFF3]' />
-            </div>
-            <div className="flex flex-col gap-1">
-                <label className='text-black'>Familiya</label>
-                <input type="text" className='w-full h-[45px] rounded-md outline-none p-1 bg-[#EBEFF3]' />
-            </div>
-            <div className="flex flex-col gap-1">
-                <label className='text-black'>Telefon raqam</label>
-                <input type="text" className='w-full h-[45px] rounded-md outline-none p-1 bg-[#EBEFF3]' />
-            </div>
-            <div className="flex flex-col gap-1">
-                <label className='text-black'>Elektronpochta</label>
-                <input type="email" className='w-full h-[45px] rounded-md outline-none p-1 bg-[#EBEFF3]' />
-            </div>
-        </form>
-            <div className="mt-3 flex flex-col gap-1">
-                <label className='text-black'>O'zingizni fikringizni qoldiring</label>
-                <textarea className='w-full resize-none h-[95px] rounded-md outline-none p-1 bg-[#EBEFF3] lg:w-[60%]'></textarea>
-            </div>
-            <button className='w-full mt-5 bg-[#15509E] border-[1px] border-[#15509E] h-[42px] rounded-md text-white lg:w-[300px] hover:bg-transparent hover:text-[#15509E]'>Yuborish</button>
+        <div className="flex flex-col gap-1">
+          <label className="text-black">Familiya</label>
+          <input type="text" className="w-full h-[45px] rounded-md outline-none p-1 bg-[#EBEFF3]" />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label className="text-black">Telefon raqam</label>
+          <input type="text" className="w-full h-[45px] rounded-md outline-none p-1 bg-[#EBEFF3]" />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label className="text-black">Elektronpochta</label>
+          <input type="email" className="w-full h-[45px] rounded-md outline-none p-1 bg-[#EBEFF3]" />
+        </div>
+      </form>
+      <div className="mt-3 flex flex-col gap-1">
+        <label className="text-black">O&apos;zingizni fikringizni qoldiring</label>
+        <textarea className="w-full resize-none h-[95px] rounded-md outline-none p-1 bg-[#EBEFF3] lg:w-[60%]"></textarea>
+      </div>
+      <button className="w-full mt-5 bg-[#15509E] border-[1px] border-[#15509E] h-[42px] rounded-md text-white lg:w-[300px] hover:bg-transparent hover:text-[#15509E]">
+        Yuborish
+      </button>
     </div>
   )
 }
--- components/map.tsx
+++ components/map.tsx
@@ -9,41 +9,39 @@ import './style.css'
 
 const Map = () => {
   return (
-    <div className='map flex items-center justify-center lg:items-start p-2 pt-5'>
-        <div className="flex flex-col gap-5 lg:flex-row">
+    <div className="map flex items-center justify-center lg:items-start p-2 pt-5">
+      <div className="flex flex-col gap-5 lg:flex-row">
         <div className="w-[90%] h-auto bg-white md:h-[60vh] p-12">
-            <div className="flex flex-col gap-2">
-            <h2 className='font-bold text-[20px]'>OOO “Ashyo”</h2>
+          <div className="flex flex-col gap-2">
+            <h2 className="font-bold text-[20px]">OOO “Ashyo”</h2>
             <p>Telefon raqam: <span>+998 71 123 45 56</span></p>
             <p>Elektron pochta: <span>ashyo@gmail.com</span></p>
             <p>Manzilimiz</p>
-            <span>100052, O‘zbekiston Respublikasi, Toshkent shahri, Bodomzor yo‘li 1-tor ko‘chasi, 72
-            </span>
+            <span>100052, O‘zbekiston Respublikasi, Toshkent shahri, Bodomzor yo‘li 1-tor ko‘chasi, 72</span>
             <p>Ijtimoiy tarmoqlarimiz</p>
             <div className="flex gap-3">
-                <button className='w-[60px] flex items-center justify-center h-[50px] bg-[#EBEFF3]'>
-                    <Image src={Facebook} alt='facebook logo'/>
-                </button>
-                <button className='w-[60px] flex items-center justify-center h-[50px] bg-[#EBEFF3]'>
-                    <Image src={Youtube} alt='youtube logo'/>
-                </button>
-                <button className='w-[60px] flex items-center justify-center h-[50px] bg-[#EBEFF3]'>
-                    <Image src={Telegram} alt='telegram logo'/>
-                </button>
-                <button className='w-[60px] flex items-center justify-center h-[50px] bg-[#EBEFF3]'>
-                    <Image src={Twitter} alt='twitter logo'/>
-                </button>
-                <button className='w-[60px] flex items-center justify-center h-[50px] bg-[#EBEFF3]'>
-                    <Image src={Instagram} alt='instagram logo'/>
-                </button>
+              <button className="w-[60px] flex items-center justify-center h-[50px] bg-[#EBEFF3]">
+                <Image src={Facebook} alt="facebook logo" />
+              </button>
+              <button className="w-[60px] flex items-center justify-center h-[50px] bg-[#EBEFF3]">
+                <Image src={Youtube} alt="youtube logo" />
+              </button>
+              <button className="w-[60px] flex items-center justify-center h-[50px] bg-[#EBEFF3]">
+                <Image src={Telegram} alt="telegram logo" />
+              </button>
+              <button className="w-[60px] flex items-center justify-center h-[50px] bg-[#EBEFF3]">
+                <Image src={Twitter} alt="twitter logo" />
+              </button>
+              <button className="w-[60px] flex items-center justify-center h-[50px] bg-[#EBEFF3]">
+                <Image src={Instagram} alt="instagram logo" />
+              </button>
             </div>
-            </div>
-
+          </div>
         </div>
-        <select className='w-[90%] h-[50px] p-2'>
-            <option>Do'konlarimizni izlang</option>
+        <select className="w-[90%] h-[50px] p-2">
+          <option>Do&apos;konlarimizni izlang</option>
         </select>
-        </div>
+      </div>
     </div>
   )
 }
