92c6cf388f5bdbb358a28b6fa8aed18b8c4a08ab
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2024-11-14 22:35:37 +0500

    like-button

--- components/card.tsx
+++ components/card.tsx
@@ -10,25 +10,25 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, image, price, credit }) => {
   return (
-    <div className="w-full h-[100%] flex flex-col justify-between gap-5 p-4 rounded-lg hover:cursor-pointer group relative">
-    <button className="absolute top-7 right-10 w-8 h-8 flex items-center justify-center bg-white text-gray-500 rounded-full shadow-md hover:bg-red-500 hover:text-white transition-colors duration-300">
-  <i className="fa-solid fa-heart"></i>
-</button>
-<Image
-  className="bg-[#EBEFF3] rounded-lg w-full transition-transform duration-300 group-hover:scale-105"
-  src={image}
-  layout="intrinsic"
-  width={400}
-  height={500}
-  alt="image"
-/>
+    <div className="w-full h-[100%] flex flex-col justify-between gap-5 p-4 rounded-lg hover:cursor-pointer group relative transition-transform duration-300">
+      <button className="absolute top-5 right-8 w-10 h-10 flex items-center justify-center bg-white text-gray-500 rounded-full shadow-md hover:bg-red-500 hover:text-white transition-colors duration-300 z-10">
+        <i className="fa-solid fa-heart"></i>
+      </button>
 
+      <Image
+        className="bg-[#EBEFF3] rounded-lg w-full transition-transform duration-300 group-hover:scale-105 z-0"
+        src={image}
+        layout="intrinsic"
+        width={400}
+        height={500}
+        alt="image"
+      />
 
-      <div className="flex flex-col gap-5 justify-between">
-        <h1 className='text-[14px] xl:text-[18px]'>{title}</h1>
+      <div className="flex flex-col gap-5 justify-between z-10">
+        <h1 className="text-[14px] xl:text-[18px]">{title}</h1>
         <div className="flex justify-between gap-2 min-[450px]:flex-col min-[686px]:flex-row">
           <div>
-            <h2 className='font-bold text-[16px] xl:text-[22px]'>{price}</h2>
+            <h2 className="font-bold text-[16px] xl:text-[22px]">{price}</h2>
             <button className="bg-[#FDEAF4] w-[151px] h-[28px] text-[13px] rounded-md">{credit}</button>
           </div>
           <div className="flex gap-2 mt-2">
